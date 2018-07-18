649e66952a0e50832f3dce9a39a1e5ad1ca00d24
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2018-07-18 11:04:53 -0500

    Just started on Gladiators using OOP

---
+++ core.py
@@ -0,0 +1,16 @@
+class Gladiator:
+    def __init__(self, name, health, rage, attack_high, attack_low):
+        self.name = name
+        self.health = health
+        self.rage = rage
+        self.attack_high = attack_high
+        self.attack_low = attack_low
+
+    def __str__(self):
+        return '{}\n----------\nHealth: {}\nRage: {}\n----------'.format(
+            self.name, self.health, self.rage)
+
+    def __repr__(self):
+        return 'Gladiator({}, {}, {}, {}, {})'.format(
+            self.name, self.health, self.rage, self.attack_high,
+            self.attack_low)
